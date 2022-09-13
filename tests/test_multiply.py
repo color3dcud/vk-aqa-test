1c8bc3d4f98e7255b898baf9866a5ecb1dd20e24
Danil Vorobev <dvorobiev@agentapp.ru>
2022-09-13 17:28:56 +0300

    add case - multiply with zero result

--- tests/test_multiply.py
+++ tests/test_multiply.py
@@ -3,10 +3,11 @@ import pytest
 from float import multiply
 
 
-@pytest.mark.parametrize("a, b, positive_result", [(10.0, 2.0, 20.0),
-                                                   (-5.0, -6.0, 30.0)])
-def test_multiply_positive_result(a, b, positive_result):
-    assert multiply(a, b) == positive_result
+@pytest.mark.parametrize("a, b, result", [(10.0, 2.0, 20.0),
+                                          (-5.0, -6.0, 30.0),
+                                          (1.0, 0.0, 0.0)])
+def test_multiply_not_negative_result(a, b, result):
+    assert multiply(a, b) == result
 
 
 @pytest.mark.parametrize("a, b, negative_result", [(10.0, -2.0, -20.0),
